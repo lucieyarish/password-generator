5d7b7843d826f66d82b38b5c6375da618a5ab109
Lucie Yarish <lucieyarish@hotmail.com>
2023-10-13 06:45:29 +0200

    Add select password length feature and user input validations

--- js/index.js
+++ js/index.js
@@ -4,14 +4,37 @@ document
   .querySelector('#generate-passwords')
   .addEventListener('click', generatePasswords);
 
-let generatedPasswordEl1 = document.getElementById('generated-password-el-1');
-let generatedPasswordEl2 = document.getElementById('generated-password-el-2');
+const generatedPasswordEl1 = document.getElementById('generated-password-el-1');
+const generatedPasswordEl2 = document.getElementById('generated-password-el-2');
+const userInputEl = document.getElementById('user-password-length');
+const errorMsgEl = document.getElementById('error-msg');
+let userInput = 0;
+
+userInputEl.addEventListener('blur', () => {
+  userInput = userInputEl.value;
+  console.log(userInput);
+});
 
 function generatePasswords() {
   resetPreviousPasswords();
-  for (let i = 0; i < 15; i++) {
-    generatedPasswordEl1.textContent += getRandomSymbol();
-    generatedPasswordEl2.textContent += getRandomSymbol();
+  let isPasswordLengthValid = validateInput();
+  console.log(isPasswordLengthValid);
+
+  if (!isPasswordLengthValid) {
+    return;
+  }
+
+  if (userInput !== 0) {
+    for (let i = 0; i < userInput; i++) {
+      generatedPasswordEl1.textContent += getRandomSymbol();
+      generatedPasswordEl2.textContent += getRandomSymbol();
+      userInputEl.value = '';
+    }
+  } else {
+    for (let i = 0; i < 15; i++) {
+      generatedPasswordEl1.textContent += getRandomSymbol();
+      generatedPasswordEl2.textContent += getRandomSymbol();
+    }
   }
 }
 
@@ -23,4 +46,17 @@ function getRandomSymbol() {
 function resetPreviousPasswords() {
   generatedPasswordEl1.textContent = '';
   generatedPasswordEl2.textContent = '';
+}
+
+function validateInput() {
+  if (userInput.length !== 0 && userInput > 20) {
+    errorMsgEl.textContent = 'Required maximum password lenght is 20.';
+    return false;
+  } else if (userInput < 8) {
+    errorMsgEl.textContent = 'Required minimum password lenght is 8.';
+    return false;
+  } else {
+    errorMsgEl.textContent = '';
+    return true;
+  }
 }
