dbde28db57f3e3286d1c9162385be70edbde609c
Lucie Yarish <lucieyarish@hotmail.com>
2023-10-14 08:10:04 +0200

    Feature: copy to clipboard; bug fixes; styles adjustments

--- js/index.js
+++ js/index.js
@@ -4,6 +4,14 @@ document
   .querySelector('#generate-passwords')
   .addEventListener('click', generatePasswords);
 
+document
+  .querySelector('#copy-btn-1')
+  .addEventListener('click', copyToClipboard);
+
+document
+  .querySelector('#copy-btn-2')
+  .addEventListener('click', copyPass2ToClipboard);
+
 const generatedPasswordEl1 = document.getElementById('generated-password-el-1');
 const generatedPasswordEl2 = document.getElementById('generated-password-el-2');
 const userInputEl = document.getElementById('user-password-length');
@@ -12,7 +20,6 @@ let userInput = 0;
 
 userInputEl.addEventListener('blur', () => {
   userInput = userInputEl.value;
-  console.log(userInput);
 });
 
 function generatePasswords() {
@@ -27,10 +34,11 @@ function generatePasswords() {
     for (let i = 0; i < userInput; i++) {
       generatedPasswordEl1.textContent += getRandomSymbol();
       generatedPasswordEl2.textContent += getRandomSymbol();
-      userInputEl.value = '';
     }
+    resetUserInput();
   } else {
     for (let i = 0; i < 15; i++) {
+      userInputEl.value = '';
       generatedPasswordEl1.textContent += getRandomSymbol();
       generatedPasswordEl2.textContent += getRandomSymbol();
     }
@@ -47,6 +55,10 @@ function resetPreviousPasswords() {
   generatedPasswordEl2.textContent = '';
 }
 
+function resetUserInput() {
+  userInput = 0;
+}
+
 function validateInput() {
   if (userInput !== 0 && userInput > 20) {
     errorMsgEl.textContent = 'Required maximum password lenght is 20.';
@@ -58,4 +70,55 @@ function validateInput() {
     errorMsgEl.textContent = '';
     return true;
   }
+}
+
+function copyToClipboard() {
+  if (
+    generatedPasswordEl1.innerText !== undefined ||
+    generatedPasswordEl1.innerText.length !== 0
+  ) {
+    copyPass1ToClipboard();
+  } else if (
+    generatedPasswordEl2.innerText !== undefined ||
+    generatedPasswordEl2.innerText.length !== 0
+  ) {
+    copyPass2ToClipboard();
+  } else {
+    return;
+  }
+}
+
+function copyPass1ToClipboard() {
+  navigator.clipboard
+    .writeText(generatedPasswordEl1.innerText)
+    .then(() => {
+      alertUser(generatedPasswordEl1.innerText);
+    })
+    .catch((err) => {
+      console.error(`Error copying text to clipboard: ${err}`);
+    });
+}
+
+function copyPass2ToClipboard() {
+  navigator.clipboard
+    .writeText(generatedPasswordEl2.innerText)
+    .then(() => {
+      alertUser(generatedPasswordEl2.innerText);
+    })
+    .catch((err) => {
+      console.error(`Error copying text to clipboard: ${err}`);
+    });
+}
+
+function alertUser(password) {
+  if (password.length === 0) {
+    return;
+  }
+  var copyMsgEl = document.getElementById('copy-msg');
+  copyMsgEl.textContent = `${password} was copied to your clipboard!`;
+  copyMsgEl.className = 'show';
+
+  setTimeout(() => {
+    copyMsgEl.className = copyMsgEl.className.replace('show', '');
+  }, 2000);
 }
